b386babc26efc6c4233f733d515888fe5fd1f3bb
angelapriyadharshini <angela.amarapala@gmail.com>
2019-11-04 01:39:40 +0530

    perf: refactor code

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AboutService } from '../../services/about-service/about.service';
+import { GlobalConstants } from 'src/app/global.constants';
 
 @Component({
   selector: 'app-about',
@@ -7,16 +8,23 @@ import { AboutService } from '../../services/about-service/about.service';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
-  about: any = [];
-  picture:any;
 
-  constructor(private aboutService: AboutService ) { }
+  imagePrefix: string;
+  imageExt: string;
+  logos: string[] = [];
+  about: any = [];
+  picture: any;
+
+  constructor(private aboutService: AboutService) { }
 
   ngOnInit() {
     this.aboutService.getAbout().subscribe(data => {
       this.about = data;
       this.picture = data.profileimage;
+      this.imagePrefix = './assets/images/';
+      this.imageExt = '.png';
+      this.logos = GlobalConstants.ABOUT_PAGE.LOGOS;
+      console.log(this.logos);
     });
   }
-
 }
